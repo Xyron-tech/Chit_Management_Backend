700e08aa23666e6d35274fca03ed3fffd0ecc1a2
Sathish <sathish.ragupathi.r@gmail.com>
2026-07-27 20:33:03 +0530

    fix

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const { getAnalytics } = require('../controllers/chitAnalyticsController');
+const { getAnalytics } = require('../controllers/chitanalyticscontroller');
 
 router.use(protect);
 
